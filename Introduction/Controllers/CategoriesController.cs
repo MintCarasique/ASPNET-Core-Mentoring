bcc7e18f109a8d295c288dfc9533292eb629f130
Artsiom Slizheuski <mykew98@gmail.com>
2019-10-12 15:34:26 +0300

    Added database connection

---
+++ Introduction/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Introduction.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Introduction.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly NorthwindContext _dbContext;
+
+        public CategoriesController(NorthwindContext context)
+        {
+            _dbContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categoriesList = _dbContext.categories.ToList();
+
+            return View(categoriesList);
+        }
+    }
+}
--- Introduction/Startup.cs
+++ Introduction/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Introduction.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,6 +26,8 @@ namespace Introduction
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("NorthwndConf");
+            services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connectionString));
             services.AddMvc();
         }
 
