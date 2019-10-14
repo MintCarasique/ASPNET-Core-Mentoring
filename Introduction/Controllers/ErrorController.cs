e49d41ac7b4188f891eb0be259c9fb3ab4243ce9
Artsiom Slizheuski <mykew98@gmail.com>
2019-10-14 13:19:42 +0300

    Added logging to error controller

--- Introduction/Controllers/ErrorController.cs
+++ Introduction/Controllers/ErrorController.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Introduction.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult ExceptionError()
         {
             var error = this.HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
+
             ViewBag.ExceptionName = error.GetType().Name;
             ViewBag.ExceptionMessage = error.Message;
-
+            _logger.LogError($"{ViewBag.ExceptionName} was thrown! Message: {ViewBag.ExceptionMessage}");
             return View();
         }
 
