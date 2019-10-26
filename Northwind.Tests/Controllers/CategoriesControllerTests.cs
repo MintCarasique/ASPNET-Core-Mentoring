8dd6915b3805271f7707d31fb651d1fb15d06537
Artsiom Slizheuski <mykew98@gmail.com>
2019-10-26 20:41:15 +0300

    Added test methods

--- Northwind.Tests/Controllers/CategoriesControllerTests.cs
+++ Northwind.Tests/Controllers/CategoriesControllerTests.cs
@@ -11,6 +11,10 @@ namespace Northwind.Tests.Controllers
 {
     public class CategoriesControllerTests
     {
+        private CategoriesController _categoriesController;
+
+        private Mock<CategoryService> _categoryServiceMock;
+
         [Fact]
         public void Index_ShouldReturnViewResult_ShouldReturnModel()
         {
@@ -66,12 +70,53 @@ namespace Northwind.Tests.Controllers
             categoryServiceMock.Verify(_ => _.UpdateCategory(category), Times.Never);
         }
 
+        [Fact]
+        public void Edit_IfIdValid_ShouldReturnView()
+        {
+            // arrange
+            var category = new Category { CategoryID = 1, CategoryName = "Test" };
+            var categoryServiceMock = ICategoryService(new List<Category> { category });
+            var controller = new CategoriesController(categoryServiceMock.Object);
+
+            // act
+            var result = controller.Edit(1);
+
+            // assert
+            Assert.IsType<ViewResult>(result);
+            categoryServiceMock.Verify(_ => _.GetCategory(1), Times.Once);
+        }
+
+        [Fact]
+        public void Edit_IfIdInvalid_ShouldRedirectToAction()
+        {
+            // arrange
+            var category = new Category { CategoryID = 1, CategoryName = "Test" };
+            var categoryServiceMock = ICategoryService(new List<Category> { category });
+            var controller = new CategoriesController(categoryServiceMock.Object);
+
+            // act
+            var result = controller.Edit(0);
+
+            // assert
+            Assert.IsType<RedirectToActionResult>(result);
+            categoryServiceMock.Verify(_ => _.GetCategory(0), Times.Once);
+        }
+
         private Mock<ICategoryService> ICategoryService(List<Category> categoriesList)
         {
             var categoryService = new Mock<ICategoryService>();
+            categoryService.Setup(_ => _.GetCategory(1)).Returns(categoriesList.FirstOrDefault);
+            categoryService.Setup(_ => _.GetCategory(0)).Returns((Category) null);
             categoryService.Setup(_ => _.GetAllCategories()).Returns(categoriesList);
             categoryService.Setup(_ => _.UpdateCategory(categoriesList.FirstOrDefault()));
             return categoryService;
+        }
+
+        private void InitializeController()
+        {
+            var category = new Category { CategoryID = 1, CategoryName = "Test" };
+            var categoryServiceMock = ICategoryService(new List<Category> { category });
+            _categoriesController = new CategoriesController(categoryServiceMock.Object);
         }
     }
 }
