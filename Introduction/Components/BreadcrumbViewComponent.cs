7819742895388eea2c57e6039d9e90713d6a5177
Artsiom Slizheuski <mykew98@gmail.com>
2019-11-17 21:24:22 +0300

    Added breadcrumb viewComponent

---
+++ Introduction/Components/BreadcrumbViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Northwind.Components
+{
+    public class BreadcrumbViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.Controller = ViewContext.RouteData.Values["controller"];
+            ViewBag.Action = ViewContext.RouteData.Values["action"];
+            return View();
+        }
+    }
+}
