02fed199a4b5355737527971b732800e653bed36
Artsiom_Slizheuski <mykew98@gmail.com>
2019-12-02 10:11:02 +0300

    Added generated client

---
+++ Northwind.GeneratedClientTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Northwind.GeneratedClientTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
