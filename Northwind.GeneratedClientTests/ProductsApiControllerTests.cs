216fd736680593fe19aedfd18c604c0e9284a644
Artsiom_Slizheuski <mykew98@gmail.com>
2019-12-02 10:29:44 +0300

    Added more unit tests

--- Northwind.GeneratedClientTests/ProductsApiControllerTests.cs
+++ Northwind.GeneratedClientTests/ProductsApiControllerTests.cs
@@ -1,23 +1,24 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using Xunit;
 
 namespace Northwind.GeneratedClientTests
 {
-    public class CategoriesApiControllerTests
+    public class ProductsApiControllerTests
     {
         [Fact]
-        public void GetAllCategories_ShouldReturnNotNull()
+        public void GetAllProducts_ShouldReturnNotNull()
         {
             var http = new HttpClient();
-            var client = new CategoriesApiClient(http);
-            IEnumerable<Category> result = client.GetAllAsync().Result;
+            var client = new ProductsApiClient(http);
+            IEnumerable<Product> result = client.GetAllAsync().Result;
             Assert.NotNull(result);
         }
 
         [Fact]
-        public void GetCategoryById_IfIdValid_ShouldReturnCategory()
+        public void GetProductById_IfIdValid_ShouldReturnProduct()
         {
             var http = new HttpClient();
             var client = new CategoriesApiClient(http);
