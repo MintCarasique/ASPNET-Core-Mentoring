f9c1b6cb79066623a67ac177fd458de40c9708f0
Artsiom_Slizheuski <mykew98@gmail.com>
2019-12-16 13:48:55 +0300

    Added role functionality

--- Introduction/Controllers/AdminController.cs
+++ Introduction/Controllers/AdminController.cs
@@ -5,22 +5,72 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Northwind.ViewModels;
 
 namespace Northwind.Controllers
 {
     //[Authorize(Roles = "admin")]
     public class AdminController : Controller
     {
-        private UserManager<IdentityUser> _userManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AdminController(UserManager<IdentityUser> userManager)
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _userManager = userManager;
+
+            _roleManager = roleManager;
         }
+
         public IActionResult Index()
         {
             List<IdentityUser> usersList = _userManager.Users.ToList();
             return View(usersList);
+        }
+
+        public async Task<IActionResult> Edit(string userId)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var allRoles = _roleManager.Roles.ToList();
+                ChangeRoleViewModel model = new ChangeRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserEmail = user.Email,
+                    UserRoles = userRoles,
+                    AllRoles = allRoles
+                };
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string userId, List<string> roles)
+        {
+            IdentityUser user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+
+                var allRoles = _roleManager.Roles.ToList();
+
+                var addedRoles = roles.Except(userRoles);
+
+                var removedRoles = userRoles.Except(roles);
+
+                await _userManager.AddToRolesAsync(user, addedRoles);
+
+                await _userManager.RemoveFromRolesAsync(user, removedRoles);
+
+                return RedirectToAction("Index");
+            }
+
+            return NotFound();
         }
     }
 }
---
+++ Introduction/ViewModels/ChangeRoleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace Northwind.ViewModels
+{
+    public class ChangeRoleViewModel
+    {
+        public string UserId { get; set; }
+        public string UserEmail { get; set; }
+        public IList<string> UserRoles { get; set; }
+        public List<IdentityRole> AllRoles { get; set; }
+    }
+}
