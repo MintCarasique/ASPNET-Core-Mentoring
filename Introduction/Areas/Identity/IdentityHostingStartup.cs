137f60091700a5f0eefc9c017c8af23366e98d08
Artsiom_Slizheuski <mykew98@gmail.com>
2019-12-16 13:17:10 +0300

    Added admin controller

--- Introduction/Areas/Identity/IdentityHostingStartup.cs
+++ Introduction/Areas/Identity/IdentityHostingStartup.cs
@@ -22,6 +22,7 @@ namespace Northwind.Areas.Identity
                         context.Configuration.GetConnectionString("IdentityContextConnection")));
 
                 services.AddDefaultIdentity<IdentityUser>()
+                    .AddRoles<IdentityRole>()
                     .AddEntityFrameworkStores<IdentityContext>()
                     .AddDefaultTokenProviders();
                 services.ConfigureApplicationCookie(options =>
---
+++ Introduction/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Northwind.Controllers
+{
+    //[Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        private UserManager<IdentityUser> _userManager;
+
+        public AdminController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            List<IdentityUser> usersList = _userManager.Users.ToList();
+            return View(usersList);
+        }
+    }
+}
